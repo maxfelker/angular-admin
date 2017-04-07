c951676f4612b92caad91906200b9e11a77f97a7
Max Felker <max@bigroomstudios.com>
2017-04-06 21:44:03 -0400

    Added delete methods to base controller

--- app/scripts/controllers/crud-base-controller.js
+++ app/scripts/controllers/crud-base-controller.js
@@ -21,7 +21,6 @@ angular.module('angularAdmin')
     this.initSuccess = function() {
       $this.viewIsReady = true;
       $scope.$parent.viewIsReady = true;
-      console.log('init success');
     };
 
     this.isActionState = function(actionState) {
@@ -34,8 +33,33 @@ angular.module('angularAdmin')
       messageService.error( 'There was an issue with ' + url);
     };
 
+    /* List Methods */
+
     this.getRecords = function() {
-      return crudService.get({url:'http://localhost:3000/users'});
+      return crudService.get(this.serviceConfig);
+    };
+
+    this.findRecordsIndex = function(records, id) {
+      var record = records.filter(function(record) {
+        return record.id === id;
+      })[0];
+      return records.indexOf(record);
+    };
+
+    /* --- Delete Methods --- */
+
+    this.removeRecordSuccess = function(records, id) {
+      messageService.deleted('Record' + id);
+      var index = $this.findRecordsIndex(records, id);
+      records.splice(index, 1);
+    };
+
+    this.removeRecord = function(records, id) {
+      if (window.confirm('Are you sure you want to remove this record?')) {
+        crudService.delete(id,this.serviceConfig).then(function() {
+          $this.removeRecordSuccess(records, id);
+        }, $this.errorHandler);
+      }
     };
 
   });
--- app/scripts/controllers/users.js
+++ app/scripts/controllers/users.js
@@ -13,17 +13,28 @@ angular.module('angularAdmin')
     angular.extend(this, $controller('CrudBaseControllerCtrl',{$scope:$scope}));
     var $this = this;
 
+    this.serviceConfig = {
+        url: 'http://localhost:3000/users'
+    };
+
+    this.transformUser = function(user) {
+      return {
+        id: 1, // Required for deleting and e
+        name: user.name.first + ' ' + user.name.last,
+        email: user.email,
+        profileImage: user.picture.large
+      }
+    };
+
     this.getUsers = function() {
       return $this.getRecords().then(function(response) {
         var users = angular.copy(response);
-        $scope.users = users.map(function(user){
-          return {
-            name: user.name.first + ' ' + user.name.last,
-            email: user.email,
-            profileImage: user.picture.large
-          }
-        })
+        $scope.users = users.map($this.transformUser)
       });
+    };
+
+    this.deleteUser = function(id) {
+      return $this.removeRecord($scope.users, id);
     };
 
     this.init([$this.getUsers()]);
