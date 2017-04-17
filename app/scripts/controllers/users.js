b84d9a2a5b0e86b77267b9786d1737017aa44bd9
Max Felker <max@bigroomstudios.com>
2017-04-17 09:42:12 -0400

    Formatting for luis 4.20

--- app/scripts/controllers/users.js
+++ app/scripts/controllers/users.js
@@ -8,33 +8,35 @@
  * Controller of the angularAdmin
  */
 angular.module('angularAdmin')
-  .controller('UsersCtrl', function ($scope,$controller, messageService, $location) {
+  .controller('UsersCtrl', function ($scope, $controller, messageService, $location) {
 
     var crudObject = {
-        name: {
-          type: 'text',
-          placeholder: 'Tom Jones',
-          value: '',
-          required: true,
-        },
-        email: {
-          type: 'text',
-          placeholder: 'youremail@host.com',
-          value: '',
-          required: true,
-        }
+      name: {
+        type: 'text',
+        placeholder: 'Tom Jones',
+        value: '',
+        required: true,
+      },
+      email: {
+        type: 'text',
+        placeholder: 'youremail@host.com',
+        value: '',
+        required: true,
+      }
     };
 
-    angular.extend(this, $controller('CrudBaseControllerCtrl',{$scope:$scope}));
+    angular.extend(this, $controller('CrudBaseControllerCtrl', {
+      $scope: $scope
+    }));
     var $this = this;
 
     // TODO: move service config to factory, pass that to controller
     this.serviceConfig = {
-        url: 'http://localhost:3000/users'
+      url: 'http://localhost:3000/users'
     };
 
     // TODO: Moved to service
-    this.transformUser = function(user) {
+    this.transformUser = function (user) {
       return {
         id: user.id,
         name: !user.name.first ? user.name : user.name.first + ' ' + user.name.last,
@@ -44,15 +46,15 @@ angular.module('angularAdmin')
 
     /* Get Users */
 
-    this.getUser = function() {
-      return $this.getRecord().then(function(response) {
+    this.getUser = function () {
+      return $this.getRecord().then(function (response) {
         var user = angular.copy(response);
         $this.setCrudObject($this.transformUser(user));
       });
     };
 
-    this.getUsers = function() {
-      return $this.getRecords().then(function(response) {
+    this.getUsers = function () {
+      return $this.getRecords().then(function (response) {
         var users = angular.copy(response);
         $scope.users = users.map($this.transformUser)
       });
@@ -60,40 +62,40 @@ angular.module('angularAdmin')
 
     /* Create Users */
 
-    this.createUser = function() {
+    this.createUser = function () {
       var payload = {
         name: {
-            first: $this.crudObject.name.value.split(' ')[0],
-            last: $this.crudObject.name.value.split(' ')[1]
+          first: $this.crudObject.name.value.split(' ')[0],
+          last: $this.crudObject.name.value.split(' ')[1]
         },
         email: $this.crudObject.email.value,
 
       };
-      return $this.createRecord(payload).then(function(user) {
-          messageService.created(user.name.first);
-          $location.path('/users/' + user.id);
+      return $this.createRecord(payload).then(function (user) {
+        messageService.created(user.name.first);
+        $location.path('/users/' + user.id);
       });
     };
 
     /* Update User */
-    this.updateUser = function() {
+    this.updateUser = function () {
       var payload = {
         name: {
-            first: $this.crudObject.name.value.split(' ')[0],
-            last: $this.crudObject.name.value.split(' ')[1]
+          first: $this.crudObject.name.value.split(' ')[0],
+          last: $this.crudObject.name.value.split(' ')[1]
         },
         email: $this.crudObject.email.value
       };
-      return $this.updateRecord(payload).then(function(user) {
-          messageService.updated(user.name.first);
-          $location.path('/users/' + user.id);
+      return $this.updateRecord(payload).then(function (user) {
+        messageService.updated(user.name.first);
+        $location.path('/users/' + user.id);
       });
     };
 
-    this.deleteUser = function(id) {
-      return $this.removeRecord(id).then( function() {
-          messageService.deleted('User ' + id);
-          $this.getUsers();
+    this.deleteUser = function (id) {
+      return $this.removeRecord(id).then(function () {
+        messageService.deleted('User ' + id);
+        $this.getUsers();
       });
     };
 
@@ -106,6 +108,6 @@ angular.module('angularAdmin')
       ]
     };
 
-    this.init(this.statePromises,crudObject);
+    this.init(this.statePromises, crudObject);
 
   });
